599ec3e57487decbdefdf14db746404f07d625bd
frehner <frehnar@hotmail.com>
2014-10-20 11:36:56 -0600

    Initial Commit

---
+++ TempleMatchingProj3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TempleMatchingProj3
+//
+//  Created by Anthony Frehner on 10/20/14.
+//  Copyright (c) 2014 Anthony Frehner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
